2dd9411d275967285e42a754081af73f8e6f3663
lucperkins <lucperkins@gmail.com>
2020-06-11 20:17:59 -0700

    Less verbose error text

--- errors.go
+++ errors.go
@@ -2,5 +2,4 @@ package rek
 
 import "errors"
 
-// The error thrown when you attempt to set the HTTP request body more than once
-var ErrRequestBodySetMultipleTimes = errors.New("you have set the request body more than once")
+var ErrRequestBodySetMultipleTimes = errors.New("request body set multiple times")
