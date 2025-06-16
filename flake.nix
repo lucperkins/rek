ff5629865900912290364288c6d7ba36ceab406f
Luc Perkins <lucperkins@gmail.com>
2025-06-16 00:09:47 -0700

    Update Nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,20 +2,15 @@
 {
   description = "rek";
 
-  inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*";
-    flake-schemas.url = "https://flakehub.com/f/DeterminateSystems/flake-schemas/*";
-  };
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
 
-  outputs = { self, nixpkgs, flake-schemas }:
+  outputs = inputs:
     let      
       supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
       });
     in {
-      schemas = flake-schemas.schemas;
-
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
